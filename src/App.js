f9e9508494e2b569e638c522d51081610163ee92
collinkip <collinskliplangat3@gmail.com>
2022-08-17 18:28:06 +0300

    front end api works

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
+import {useState} from 'react';
+import Delivery from './Delivery';
+
+
 
 function App() {
+
+  const {id,setId}=useState('');
+  console.log(id)
+
+  const submit =async (e)=>{
+    e.preventDefault();
+    const form =new FormData(e.target);
+    const data = Object.fromEntries(form.entries());
+    const response=await fetch('http://127.0.0.1:8000/deliveries/create',{
+      method:'POST',
+      headers:{'Content-Type':'application/json',  "access-control-allow-origin" : "*",
+      "Content-type": "application/json; charset=UTF-8"    
+    
+    },
+      body:JSON.stringify({
+        type:"CREATE_DELIVERY",
+        data
+      })
+    });
+
+    const {id} =await response.json();
+    setId(id)
+
+  }
+
   return (
     <div className="py-5">
       <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
@@ -10,8 +39,7 @@ function App() {
           Create delivery
         </div>
 
-
-        <form className='card-body'>
+        <form className='card-body' onSubmit={submit}>
           <div className="mb-3">
             <input type="number" name='budget' className='form-control' placeholder='Budget' />
           </div>
@@ -20,7 +48,7 @@ function App() {
           </div>
           <button className='btn btn-primary'>Submit</button>
         </form>
-      </div>
+      </div> 
       </div>
     </div>
   );
---
+++ src/Delivery.js
@@ -0,0 +1,13 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css'
+const Delivery =()=>{
+    return (
+        <div className=""> 
+            <h2>Delivery</h2>
+        </div>
+
+    );
+};
+
+export default Delivery;
+
