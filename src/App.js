7ace665d3eb371a79a626d74c60cb59553b1d52a
collinkip <collinskliplangat3@gmail.com>
2022-08-17 15:06:34 +0300

    create delivery form created

--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="py-5">
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
+      <div className="card">
+        <div className="card-header">
+          Create delivery
+        </div>
+
+
+        <form className='card-body'>
+          <div className="mb-3">
+            <input type="number" name='budget' className='form-control' placeholder='Budget' />
+          </div>
+          <div className="mb-3">
+            <textarea name='notes' className='form-control' placeholder='Notes' />
+          </div>
+          <button className='btn btn-primary'>Submit</button>
+        </form>
+      </div>
+      </div>
     </div>
   );
 }
